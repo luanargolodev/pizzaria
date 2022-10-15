378aa53da5b42e0cd1b3fe8fbd03c90eae618880
Luan Argolo <ipsluan@icloud.com>
2022-10-15 14:19:34 -0300

    Listando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -15,5 +16,6 @@ router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
