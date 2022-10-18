ee51fcabdfdefe75340293dfc5ce411f634dd1da
Luan Argolo <ipsluan@icloud.com>
2022-10-17 23:31:26 -0300

    Criar página de cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,25 +1,32 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import logoImg from '../../public/logo.svg';
+import logoImg from '../../../public/logo.svg';
 
-import { Input } from '../components/ui/Input';
-import { Button } from '../components/ui/Button';
+import { Input } from '../../components/ui/Input';
+import { Button } from '../../components/ui/Button';
 
 import Link from 'next/link';
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Sujeito Pizza - Faça seu login</title>
+        <title>Sujeito Pizza - Registre sua empresa</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+          <Input
+              placeholder="Digite seu nome"
+              type="text"
+            />
+
             <Input
               placeholder="Digite seu e-mail"
               type="text"
@@ -34,12 +41,12 @@ export default function Home() {
               type="submit"
               loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href="/signup">
-            <a className={styles.text}>Ainda não possui uma conta? Cadastre sua empresa agora</a>
+          <Link href="/">
+            <a className={styles.text}>Já possui uma conta? Entre agora mesmo</a>
           </Link>
         </div>
       </div>
