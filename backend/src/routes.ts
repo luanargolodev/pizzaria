eefff0dd275621f73b8c1919670c562c22387455
Luan Argolo <ipsluan@icloud.com>
2022-10-17 21:44:08 -0300

    Listar últimos pedidos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,6 +18,8 @@ import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 
+import { ListOrdersController } from './controllers/order/ListOrdersController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 import uploadConfig from './config/multer'
@@ -42,6 +44,8 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
-router.put('/order/send', isAuthenticated, new SendOrderController().handle);	
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+
+router.get('/orders', isAuthenticated, new ListOrdersController().handle);
 
 export { router };
