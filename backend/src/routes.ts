18e9df4b5e57010c4cbd7ba07bb058ec14b5efee
Luan Argolo <ipsluan@icloud.com>
2022-10-17 22:05:13 -0300

    Finalizar pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -20,6 +20,7 @@ import { SendOrderController } from './controllers/order/SendOrderController';
 
 import { ListOrdersController } from './controllers/order/ListOrdersController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -49,5 +50,6 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 
 router.get('/orders', isAuthenticated, new ListOrdersController().handle);
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
 
 export { router };
