2d00260f734b78e22b6ab22b73589b93b2c52b31
Luan Argolo <ipsluan@icloud.com>
2022-10-17 22:43:40 -0300

    Criar document

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito Pizza</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sujeito Pizza - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>Sujeito Pizza</h1>
+      </div>
+    </>
   )
 }
