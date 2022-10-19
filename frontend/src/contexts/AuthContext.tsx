bf2ba2d7e9a64d73cc2823baa2e5f8d936fa4bdd
Luan Argolo <ipsluan@icloud.com>
2022-10-19 00:56:48 -0300

    Cadastrar usuário

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ type AuthContextData = {
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
   signOut: () => void;
+  signUp: (credentials: SignUpProps) => Promise<void>;
 }
 
 type UserProps = {
@@ -17,6 +18,12 @@ type UserProps = {
 }
 
 type SignInProps = {
+  email: string;
+  password: string;
+}
+
+type SignUpProps = {
+  name: string;
   email: string;
   password: string;
 }
@@ -68,8 +75,25 @@ export function AuthProvider({children}: AuthProviderProps) {
     }
   }
 
+  async function signUp({name, email, password}: SignUpProps) {
+    try {
+      const response = await api.post('/users', {
+        name,
+        email,
+        password
+      });
+
+      console.log('cadastrado com sucesso!');
+
+      Router.push('/');
+
+    } catch(err) {
+      console.log('Erro ao cadastrar usuário ', err);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut}}>
+    <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut, signUp}}>
       {children}
     </AuthContext.Provider>
   )
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,3 +1,4 @@
+import { useState, FormEvent, useContext } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../../styles/home.module.scss';
@@ -7,9 +8,39 @@ import logoImg from '../../../public/logo.svg';
 import { Input } from '../../components/ui/Input';
 import { Button } from '../../components/ui/Button';
 
+import { AuthContext } from '../../contexts/AuthContext';
+
 import Link from 'next/link';
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+
+    if(name === '' || email === '' || password === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      name,
+      email,
+      password
+    };
+
+    await signUp(data);
+
+    setLoading(false);
+  }
+
   return (
     <>
       <Head>
@@ -21,25 +52,31 @@ export default function SignUp() {
         <div className={styles.login}>
           <h1>Criando sua conta</h1>
 
-          <form>
-          <Input
+          <form onSubmit={handleSignUp}>
+            <Input
               placeholder="Digite seu nome"
               type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
 
             <Input
               placeholder="Digite seu e-mail"
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <Input
               placeholder="Digite sua senha"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
             >
               Cadastrar
             </Button>
