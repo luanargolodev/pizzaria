45f67b4652577ff2ee820344e318e546f40305c8
Luan Argolo <ipsluan@icloud.com>
2022-10-22 17:44:10 -0300

    Controlar rotas privadas

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,6 +14,8 @@ import { toast } from 'react-toastify';
 
 import Link from 'next/link';
 
+import { canSSRGuest } from '../utils/canSSRGuest';
+
 export default function Home() {
   const { signIn } = useContext(AuthContext);
   const [email, setEmail] = useState('')
@@ -80,3 +82,9 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async () => {
+  return {
+    props: {}
+  }
+})
