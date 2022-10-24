b9331d56588b907679e9a763bf9f7720ee587c09
Luan Argolo <ipsluan@icloud.com>
2022-10-23 23:06:53 -0300

    Adicionar funcionalidade ao login

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,10 +3,13 @@ import React, {
   createContext,
   ReactNode
 } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { api } from '../services/api';
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
+  signIn: (credentials: SignInProps) => Promise<void>;
 }
 
 type UserProps = {
@@ -20,6 +23,11 @@ type AuthProviderProps = {
   children: ReactNode;
 }
 
+type SignInProps = {
+  email: string;
+  password: string;
+}
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({children}: AuthProviderProps) {
@@ -30,9 +38,40 @@ export function AuthProvider({children}: AuthProviderProps) {
     token: ''
   });
   const isAuthenticated = !!user.name;
+  const [loadingAuth, setLoadingAuth] = useState(false);
+
+  async function signIn({email, password}: SignInProps) {
+    setLoadingAuth(true);
+
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      });
+      const { id, name, token } = response.data;
+      const data = {
+        ...response.data
+      };
+
+      await AsyncStorage.setItem('@sujeitopizza', JSON.stringify(data));
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      setUser({
+        id,
+        name,
+        email,
+        token
+      });
+      setLoadingAuth(false);
+    } catch(err) {
+      console.log('Erro ao acessar', err);
+      setLoadingAuth(false);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{user, isAuthenticated}}>
+    <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
       {children}
     </AuthContext.Provider>
   )
